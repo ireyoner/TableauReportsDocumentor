ae340c284e204a695a2951b0054cfb9baa5d1717
Marcin Blaszyk <haldiraros@gmail.com>
2015-04-01 22:44:46 +0200

    Some rudimentary tables in docx
    The info is not yet limited. Writing almost everything that is found

--- TableauReportsDocumentor/TableauReportsDocumentor/Export Converters/DocxExport.cs
+++ TableauReportsDocumentor/TableauReportsDocumentor/Export Converters/DocxExport.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Novacode;
+using System.Xml;
 
 namespace TableauReportsDocumentor.Export_Converters
 {
@@ -17,14 +18,106 @@ namespace TableauReportsDocumentor.Export_Converters
 
         public bool export(string exportFileName, System.Xml.XmlDocument exportSource)
         {
+
+
+            XmlNode root = exportSource.DocumentElement;
+            string text;
+            XmlNode nodeTest;
             using (DocX document = DocX.Create(exportFileName))
             {
-                Paragraph p = document.InsertParagraph("tests");
+                Paragraph p = document.InsertParagraph("Docx Test Document");
 
+                foreach (XmlNode section in root.SelectNodes("//section"))
+                {
+                    nodeTest =  section.SelectSingleNode("text");
+                    if ( nodeTest == null ){
+                        text = string.Format("{0}", section.SelectSingleNode("title").InnerText);
+                    }else{
+                        text = string.Format("{0} -- {1}", section.SelectSingleNode("title").InnerText, nodeTest.InnerText);
+                    }
+                    p = document.InsertParagraph();
+                    p.AppendLine(text);
+
+                    foreach (XmlNode subsection in section.SelectNodes("subsection"))
+                    {
+
+                        p = document.InsertParagraph(); 
+
+                        nodeTest = subsection.SelectSingleNode("text");
+                        if (nodeTest == null)
+                        {
+                            text = string.Format("{0}", subsection.SelectSingleNode("title").InnerText);
+                        }
+                        else
+                        {
+                            text = string.Format("{0} -- {1}", subsection.SelectSingleNode("title").InnerText, nodeTest.InnerText);
+                        }
+                        p.AppendLine(text);
+
+                        foreach (XmlNode table in subsection.SelectNodes("table"))
+                        {
+                            p = document.InsertParagraph(); 
+                            p.AppendLine(table.SelectSingleNode("title").InnerText);
+
+                            XmlNodeList headers = table.SelectNodes("header/cell");
+                            int colCount = headers.Count;
+                            Table t =document.InsertTable(1, colCount);
+                            t.Design = TableDesign.ColorfulListAccent1;
+                            int i;
+                            for (i = 0; i < colCount; i++ )
+                            {
+                                t.Rows[0].Cells[i].InsertParagraph(headers[i].InnerText);
+                            }
+
+                            XmlNodeList rows = table.SelectNodes("row");
+                            foreach (XmlNode row in rows)
+                            {
+                                Row newTabRow = t.InsertRow();
+                                 for (i = 0; i < colCount; i++ )
+                                 {
+                                    newTabRow.Cells[i].InsertParagraph(row.ChildNodes[i].InnerText);
+                                 }
+
+                            }
+
+                        }
+
+                    }
+
+                    foreach (XmlNode table in section.SelectNodes("table"))
+                    {
+                        p = document.InsertParagraph();
+                        p.AppendLine(table.SelectSingleNode("title").InnerText);
+
+                        XmlNodeList headers = table.SelectNodes("header/cell");
+                        int colCount = headers.Count;
+                        Table t = document.InsertTable(1, colCount);
+                        t.Design = TableDesign.ColorfulListAccent1;
+                        int i;
+                        for (i = 0; i < colCount; i++)
+                        {
+                            t.Rows[0].Cells[i].InsertParagraph(headers[i].InnerText);
+                        }
+
+                        XmlNodeList rows = table.SelectNodes("row");
+                        foreach (XmlNode row in rows)
+                        {
+                            Row newTabRow = t.InsertRow();
+                            for (i = 0; i < colCount; i++)
+                            {
+                                newTabRow.Cells[i].InsertParagraph(row.ChildNodes[i].InnerText);
+                            }
+
+                        }
+
+                    }
+                }
                 document.Save();
             }
+
             //throw new NotImplementedException();
             return true;
         }
+
     }
 }
